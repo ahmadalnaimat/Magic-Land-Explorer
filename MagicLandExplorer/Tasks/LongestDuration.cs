28c299ff1ef615cbf1b5fc8e1367f37553f6aaa7
Ahmad Alfaza'at <23039673@student.ltuc.com>
2024-07-22 19:31:56 +0300

    done?

---
+++ MagicLandExplorer/Tasks/LongestDuration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicLandExplorer.Tasks
+{
+    public class LongestDuration
+    {
+
+        public static Destination HighestDuration(List<Destination> destinations)
+        {
+            return destinations.OrderByDescending(a => DurationinMin(a.Duration)).FirstOrDefault();
+        }
+        private static int DurationinMin(string Duration)
+        {
+            if (string.IsNullOrWhiteSpace(Duration))
+            {
+                return 0; 
+            }
+            if (int.TryParse(Duration.Split(" ")[0], out int minutes))
+            {
+                return minutes;
+            }
+            return 0;
+        }
+    }
+}
